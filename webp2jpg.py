e14c5c357cb3d86e58342a661fe362573d97e651
Marcelo Larios <marcelolarios@users.noreply.github.com>
2023-12-16 21:47:50 -0300

    Add files via upload
    atualiza na pasta corrente e em subfolders

--- webp2jpg.py
+++ webp2jpg.py
@@ -7,14 +7,20 @@ pip install libwebp
 cd /filespath/
 python3 webp2jpg.py
 '''
+import os
 from PIL import Image
-import glob
 from pathlib import Path
 
-for f in glob.glob("*.webp"):
-    filename = (Path(f).stem)
-    im = Image.open(filename + ".webp").convert("RGB")
-    im.save(filename + ".jpg","jpeg")
+for root, _, files in os.walk(os.getcwd()):
+	for f in files:
+		if f.endswith('.webp'):
+			fullname = os.path.join(root, f)
+			print (fullname)
+			caminho = (os.path.join(root))
+			filename = (Path(f).stem)
+			
+			im = Image.open(fullname).convert("RGB")
+			im.save(caminho + "/" + filename + ".jpg","jpeg")
 
 print('\nDone!')
      
